69a942605c8670ceaf783adcba979110468a43bb
nicolaiparlog <nipa@codefx.org>
2014-12-10 22:53:53 +0100

    Rename `MetaProperty.declaringClass()` to `MetaProperty.declaringType()`
    There is currently no other method which uses the word "Class" in its name; instead all use "Type". To increase consistency the method is renamed.

--- src/main/java/org/joda/pa/MetaProperty.java
+++ src/main/java/org/joda/pa/MetaProperty.java
@@ -66,7 +66,7 @@ public interface MetaProperty<P> {
      * 
      * @return the type declaring the property, not null
      */
-    default Class<?> declaringClass() {
+    default Class<?> declaringType() {
         return metaBean().beanType();
     }
 
